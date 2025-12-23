ba81bafdb7fabe5e7803f3156f10ad6faf8af96e
YVC <yoeri.vancauteren@sbe.be>
2025-12-23 22:05:44 +0100

    finished part 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,25 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                total += int(row[1]) * float(row[2])
+            except ValueError:
+                print(f"Error in reading line {row}")
+    return total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+    
+cost = portfolio_cost(filename)
+print(f"Total : {cost}")
