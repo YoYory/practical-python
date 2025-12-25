878c659f033fe63f744892096fc13523b9547557
YVC <yoeri.vancauteren@sbe.be>
2025-12-25 21:06:58 +0100

    2.12

--- Work/report.py
+++ Work/report.py
@@ -3,7 +3,7 @@
 # Exercise 2.4
 import csv
 
-def read_portfolio(filename):
+def read_portfolio(filename: str) -> list:
     portfolio = []
     with open(filename, "rt") as f:
         rows = csv.reader(f)
@@ -15,7 +15,7 @@ def read_portfolio(filename):
                 print(f"Error in reading line {row}")
     return portfolio
 
-def read_prices(filename):
+def read_prices(filename: str) -> dict:
     prices = dict()
     with open(filename, "rt") as f:
         rows = csv.reader(f)
@@ -26,6 +26,15 @@ def read_prices(filename):
                 print("Error reading line")
     return prices
 
+def make_report(portfolio: list, prices: dict) -> list[tuple]:
+    rows = []
+    for s in portfolio:
+        change =  prices[s['name']] - s['price']
+        t = (s['name'], s['shares'], s['price'], change)
+        rows.append(t)
+    return rows
+
+
 portfolio = read_portfolio('Data/portfolio.csv')
 prices = read_prices('Data/prices.csv')
 new_value = 0
@@ -33,8 +42,14 @@ old_value = 0
 for s in portfolio:
     old_value += s['shares'] * s['price']
     new_value += s['shares'] * prices[s['name']]
-
 print(f"Old value: {old_value}")
 print(f"New value: {new_value}")
-print(f"Gain/loss : {new_value-old_value}")
+print(f"Gain/loss : {new_value-old_value} \n")
+
+report = make_report(portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('{:>10s} {:>10s} {:>10s} {:>10s}'.format(*headers))
+print(("-" * 10 + " ") * len(headers))
+for name, shares, price, change in report:
+    print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
 
