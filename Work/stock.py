b4c9cbeac4de9fda9e81f176a67b38bfc36f5666
YVC <yoeri.vancauteren@sbe.be>
2025-12-31 12:39:37 +0100

    part 5.2

--- Work/stock.py
+++ Work/stock.py
@@ -1,4 +1,5 @@
 class Stock:
+    __slots__ = ("name", "_shares", "price")
     def __init__(self, name: str, shares: int, price: float):
         self.name = name
         self.shares = shares
@@ -6,7 +7,18 @@ class Stock:
     
     def __repr__(self):
         return f"Stock({self.name}, {self.shares}, {self.price})"
+    
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Shares needs to be of type int")
+        self._shares = value
 
+    @property
     def cost(self):
         return self.shares * self.price
     
